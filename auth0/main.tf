c8fcce3555953d76b5de76af57b5de93c527a0b9
Ben Jefferies <benjefferies@echosoft.uk>
2020-06-03 23:24:34 +0100

    Add / to callback and change jwt signing alg

--- auth0/main.tf
+++ auth0/main.tf
@@ -33,11 +33,14 @@ resource "auth0_client" "webapp" {
   is_first_party             = true
   token_endpoint_auth_method = "client_secret_post"
   oidc_conformant            = false
-  callbacks                  = ["https://localhost:3000", "https://dogarthritistest.co.uk/callback"]
+  callbacks                  = ["https://localhost:3000/", "https://dogarthritistest.co.uk/callback"]
   allowed_origins            = ["https://localhost:3000", "https://dogarthritistest.co.uk"]
   grant_types                = ["authorization_code", "http://auth0.com/oauth/grant-type/password-realm", "implicit", "password", "refresh_token"]
   allowed_logout_urls        = ["https://localhost:3000", "https://dogarthritistest.co.uk"]
   web_origins                = ["https://localhost:3000", "https://dogarthritistest.co.uk"]
+  jwt_configuration {
+    alg = "RS256"
+  }
 }
 
 resource "auth0_client_grant" "webapp" {
