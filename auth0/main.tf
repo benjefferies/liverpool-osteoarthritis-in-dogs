1a239b7025b2e73c1ce1350818a68951774fa2ac
Ben Jefferies <benjefferies@echosoft.uk>
2020-06-04 20:56:37 +0100

    Use plain text locally

--- auth0/main.tf
+++ auth0/main.tf
@@ -33,11 +33,11 @@ resource "auth0_client" "webapp" {
   is_first_party             = true
   token_endpoint_auth_method = "client_secret_post"
   oidc_conformant            = false
-  callbacks                  = ["https://localhost:3000", "https://dogarthritistest.co.uk/callback"]
-  allowed_origins            = ["https://localhost:3000", "https://dogarthritistest.co.uk"]
+  callbacks                  = ["http://localhost:3000", "https://dogarthritistest.co.uk/callback"]
+  allowed_origins            = ["http://localhost:3000", "https://dogarthritistest.co.uk"]
   grant_types                = ["authorization_code", "http://auth0.com/oauth/grant-type/password-realm", "implicit", "password", "refresh_token"]
-  allowed_logout_urls        = ["https://localhost:3000", "https://dogarthritistest.co.uk"]
-  web_origins                = ["https://localhost:3000", "https://dogarthritistest.co.uk"]
+  allowed_logout_urls        = ["http://localhost:3000", "https://dogarthritistest.co.uk"]
+  web_origins                = ["http://localhost:3000", "https://dogarthritistest.co.uk"]
   jwt_configuration {
     alg = "RS256"
   }
